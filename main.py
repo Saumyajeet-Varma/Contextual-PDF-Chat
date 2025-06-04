d1b61d3e158b8ede20acaf2aef34a07aa360643c
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-06-04 23:21:47 +0530

    text extraction done

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import pdfplumber
+
+app = Flask(__name__)
+
+# ---------- UTILS ----------
+def extract_text_from_pdf(filestream):
+    text = ''
+    with pdfplumber.open(filestream) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text
+    return text
+
+# ---------- ROUTES ----------
+@app.route("/")
+def index():
+    return "SERVER IS RUNNING"
+
+@app.route("/upload", methods=['POST'])
+def upload_pdf():
+
+    file = request.files['file']
+    
+    if not file:
+        return jsonify({"error": "No file uploaded"}), 400
+    
+    text = extract_text_from_pdf(file.stream)
+    
+    return jsonify({"success": True, "message": "PDF Text extracted", "text": text}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
