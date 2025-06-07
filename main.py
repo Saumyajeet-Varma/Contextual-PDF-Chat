1072d5b04872ad66a910d1e4b173a21496db18e9
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-06-07 15:58:23 +0530

    ask route created

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pdfplumber
 import textwrap
 import sqlite3
 import uuid
+import faiss
 import numpy as np
-from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 
 load_dotenv()
@@ -13,7 +14,6 @@ load_dotenv()
 app = Flask(__name__)
 
 model = SentenceTransformer(os.getenv('MODEL'))
-
 db_path = os.getenv("DB_PATH")
 
 # ---------- UTILS ----------
@@ -51,6 +51,24 @@ def store_chunks(chunks, embeddings):
     conn.commit()
     conn.close()
 
+def load_all_chunks_and_embeddings():
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute("SELECT chunk, embedding FROM documents")
+    rows = c.fetchall()
+    chunks = []
+    embeddings = []
+    for chunk, emb_blob in rows:
+        chunks.append(chunk)
+        embedding = np.frombuffer(emb_blob, dtype="float32")
+        embeddings.append(embedding)
+    conn.close()
+    return chunks, embeddings
+
+# TODO: Integrate LLM
+def get_LLM_response(question, context):
+    return context
+
 # ---------- ROUTES ----------
 @app.route("/")
 def index():
@@ -62,15 +80,41 @@ def upload_pdf():
     file = request.files['file']
     
     if not file:
-        return jsonify({"error": "No file uploaded"}), 400
+        return jsonify({"success": False, "message": "No file uploaded"}), 400
     
     text = extract_text_from_pdf(file.stream)
     chunks = chunk_text(text)
     embeddings = model.encode(chunks)
-    
+
     store_chunks(chunks, embeddings)
     
     return jsonify({"success": True, "message": "PDF processed and stored in permanent memory", "text": text}), 200
+
+@app.route("/ask", methods=['POST'])
+def get_answer():
+
+    question = request.json.get("question")
+
+    if not question:
+        return jsonify({"success": False, "message": "No question asked"}), 400
+    
+    chunks, embeddings = load_all_chunks_and_embeddings()
+
+    if not chunks:
+        return jsonify({"success": False, "message": "No documents available in the memory"}), 400
+    
+    dim = embeddings[0].shape[0]
+    index = faiss.IndexFlatL2(dim)
+    index.add(np.array(embeddings))
+
+    query_embedding = model.encode([question])
+    distance, indices = index.search(np.array(query_embedding), k=3)
+    relative_chunks = [chunks[i] for i in indices[0] if i < len(chunks)]
+
+    context = "\n".join(relative_chunks)
+    answer = get_LLM_response(question, context)
+
+    return jsonify({"success": True, "message": "Relative chunks fetched", "answer": answer}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
